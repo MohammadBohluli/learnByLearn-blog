7c3c17ff1be9fcb7a87052f500b2343a83b8b918
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-05-31 23:48:34 +0330

    create search component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div className="mx-auto max-w-3xl px-4 py-2 sm:px-6 lg:max-w-6xl xl:max-w-5xl xl:px-0">
       <Header />
+      <Search />
     </div>
   );
 };
