d1b83130b9eb740e126a1194cf7f5d0e2345a9ef
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-06-26 19:16:31 +0330

    create login form

--- frontend/src/components/RowForm.tsx
+++ frontend/src/components/RowForm.tsx
@@ -1,11 +1,16 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-const RowForm = ({ children }: Props) => {
-  return <div className="mb-5">{children}</div>;
+const RowForm = ({ children, className, ...props }: Props) => {
+  return (
+    <div className={cn("mb-5", className)} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default RowForm;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -8,6 +8,7 @@ import ProfileAdminPage from "./pages/admin/profile/ProfileAdminPage";
 import StatsAdminPage from "./pages/admin/StatsAdminPage";
 import UsersAdminPage from "./pages/admin/UsersAdminPage";
 import CreateArticleAdminPage from "./pages/admin/articles/CreateArticleAdminPage";
+import LoginPage from "./pages/auth/login-page";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
       { path: "/projects", element: <ProjectsPage /> },
     ],
   },
+  { path: "/login", element: <LoginPage /> },
   {
     path: "/profile",
     element: <AdminLayout />,
