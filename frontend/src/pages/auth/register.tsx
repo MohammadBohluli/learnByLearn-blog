c52107f491f5cb09b55b013278dd502a6a863506
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-06-28 21:22:44 +0330

    implement register component

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -2,23 +2,29 @@ import Logo from "@/components/Logo";
 import RowForm from "@/components/RowForm";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
-    <Card className="z-50 mx-2 max-w-md flex-1 py-9 shadow-lg">
+    <Card className="z-50 mx-2 max-w-md flex-1 pt-9 shadow-lg">
       <CardHeader className="text-center">
         <Logo className="text-5xl" />
       </CardHeader>
       <CardContent>
         <form>
           <RowForm>
+            <Label htmlFor="name">نام</Label>
+            <Input id="name" type="text" className="mt-1" />
+          </RowForm>
+          <RowForm>
+            <Label htmlFor="family">نام خانوادگی</Label>
+            <Input id="family" type="text" className="mt-1" />
+          </RowForm>
+          <RowForm>
             <Label htmlFor="email-login">ایمیل</Label>
             <Input
-              dir="ltr"
               id="email-login"
               type="text"
               placeholder="example@gmail.com"
@@ -26,37 +32,36 @@ export default function Register() {
             />
           </RowForm>
           <RowForm>
-            <Label htmlFor="password-login">کلمه عبور</Label>
+            <Label htmlFor="password-login1">کلمه عبور</Label>
             <Input
               dir="ltr"
-              id="password-login"
+              id="password-login1"
               type="password"
               placeholder="********"
               className="mt-1 text-inherit"
             />
           </RowForm>
-          <RowForm className="flex justify-between text-sm">
-            <div className="flex items-center gap-1">
-              <Checkbox id="remember-me" />
-              <Label className="font-normal" htmlFor="remember-me">
-                من را به یاد داشته باش
-              </Label>
-            </div>
-            <div>
-              <Link to={"/"} className="text-blue-500">
-                رمز عبورم را فراموش کردم
-              </Link>
-            </div>
+          <RowForm>
+            <Label htmlFor="password-login2">تکرار کلمه عبور</Label>
+            <Input
+              dir="ltr"
+              id="password-login2"
+              type="password"
+              placeholder="********"
+              className="mt-1 text-inherit"
+            />
           </RowForm>
-          <RowForm className="mb-2 mt-[60px]">
-            <Button variant={"default"} className="w-full text-lg">
-              ورود
-            </Button>
-          </RowForm>
+
+          <Button variant={"default"} className="mt-3 w-full text-lg">
+            ثبت نام
+          </Button>
         </form>
-        <Button variant={"secondary"} className="w-full">
-          <Link to={"/"}>ثبت نام</Link>
-        </Button>
+        <div className="mt-2 flex justify-center gap-2">
+          <p>قبلا ثبت نام کردی ؟</p>
+          <Link to={"/"} className="text-blue-500">
+            وارد شوید
+          </Link>
+        </div>
       </CardContent>
     </Card>
   );
